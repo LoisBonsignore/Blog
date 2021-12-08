60491524cc323af233ee26867680c8c739f08930
Lois Bonsignore <bonsignore.lois@gmail.com>
2021-12-08 15:53:53 +0100

    mise en page

--- src/Articles/Articles.js
+++ src/Articles/Articles.js
@@ -12,12 +12,12 @@ function Article() {
       if (parsePosts !== null)
     return parsePosts.map(function (posts) { // Créer un nouveau composant pour le maper et qu'il ait sa propre const d'état "like"
       return (
-        <div>
+        <div className="general-article-container">
           <div className="article-container">
             <h2>{posts.post.title}</h2>
             <p>{posts.post.article}</p>
             {/* <p>{date}</p> */}
-            <p>écrit par {posts.post.name}</p>
+            <p>écrit par {posts.post.firstname} {posts.post.name}</p>
             <img
               id="photo-amsterdam"
               src="https://medias.voyages-leonard.com/files/2850/p_970x620/2850-20939-amsterdam--28c-29-adobestock-201794112--281-29.jpg"
